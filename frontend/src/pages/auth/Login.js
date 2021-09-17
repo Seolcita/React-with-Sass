1fdc02378cb6d653906a37d5a13bfe9021eb5ca9
Seolcita <seolcita@gmail.com>
2021-09-16 23:34:02 -0600

    React and Sass setup

---
+++ frontend/src/pages/auth/Login.js
@@ -0,0 +1,17 @@
+/** @format */
+
+import React from 'react';
+import '../../index.scss';
+import './login.scss';
+
+const Login = () => {
+  return (
+    <div className="login">
+      <h1>Login</h1>
+      <h2 className="login__header--main"> Header Main</h2>
+      <h3 className="login__header--sub"> Header Sub</h3>
+    </div>
+  );
+};
+
+export default Login;
